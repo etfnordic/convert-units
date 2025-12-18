1cb3cf6b8ac2c74e88c7f046066144f3eab7fe9a
etfnordic <etfnordic@gmail.com>
2025-12-18 17:48:53 +0100

    Update speed.js

--- js/speed.js
+++ js/speed.js
@@ -1,1 +1,18 @@
+const speedConfig = {
+  defaultFrom:"kmh", defaultTo:"mph",
+  units:[
+    {key:"ms", label:"Meters per second (m/s)", symbol:"m/s"},
+    {key:"kmh",label:"Kilometers per hour (km/h)",symbol:"km/h"},
+    {key:"mph",label:"Miles per hour (mph)",symbol:"mph"},
+    {key:"knot",label:"Knots (kn)",symbol:"kn"},
+  ],
+  // base = m/s
+  unitMap:{
+    ms:{factor:1, symbol:"m/s"},
+    kmh:{factor:0.2777777778, symbol:"km/h"},
+    mph:{factor:0.44704, symbol:"mph"},
+    knot:{factor:0.514444, symbol:"kn"},
+  }
+};
+setupFactorConverter(speedConfig);
 
