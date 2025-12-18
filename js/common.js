3513856d1ba5be21aea2ea112ee622049f97ee71
etfnordic <etfnordic@gmail.com>
2025-12-18 17:44:27 +0100

    Update common.js

--- js/common.js
+++ js/common.js
@@ -1,1 +1,6 @@
 
+// Sätter årtal i footer
+document.addEventListener("DOMContentLoaded", () => {
+  const y = document.getElementById("year");
+  if (y) y.textContent = new Date().getFullYear();
+});
