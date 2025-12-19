547ecc75f2ca1c6e622538d035d470593753ca22
etfnordic <etfnordic@gmail.com>
2025-12-19 13:14:09 +0100

    Update time.js

--- js/time.js
+++ js/time.js
@@ -7,8 +7,8 @@ const timeConfig = {
     {key:"min",label:"Minute (min)", symbol:"min"},
     {key:"h",  label:"Hour (h)", symbol:"h"},
     {key:"day",label:"Day", symbol:"day"},
-    {key:"week",label:"Week", symbol:"week"}
-    {key:"month",label:"Month", symbol:"month"}
+    {key:"week",label:"Week", symbol:"week"},
+    {key:"month",label:"Month", symbol:"month"},
   ],
   unitMap:{
     ms:{factor:0.001, symbol:"ms"},
@@ -16,8 +16,8 @@ const timeConfig = {
     min:{factor:60, symbol:"min"},
     h:{factor:3600, symbol:"h"},
     day:{factor:86400, symbol:"day"},
-    week:{factor:604800, symbol:"week"}
-    month:{factor:2629800, symbol:"month"}
-  }
+    week:{factor:604800, symbol:"week"},
+    month:{factor:2629800, symbol:"month"},
+  },
 };
 setupFactorConverter(timeConfig);
