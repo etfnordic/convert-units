a577e6ad379293ef48d7681978df99caf9c56207
etfnordic <etfnordic@gmail.com>
2025-12-19 13:41:01 +0100

    Update speed.js

--- js/speed.js
+++ js/speed.js
@@ -4,13 +4,15 @@ const speedConfig = {
     {key:"ms", label:"Meters per second (m/s)", symbol:"m/s"},
     {key:"kmh",label:"Kilometers per hour (km/h)",symbol:"km/h"},
     {key:"mph",label:"Miles per hour (mph)",symbol:"mph"},
+    {key:"fts",label:"Feet per second (ft/s)",symbol:"ft/s"},
     {key:"knot",label:"Knots (kn)",symbol:"kn"},
   ],
-  // base = m/s
+  // base = m/s 
   unitMap:{
     ms:{factor:1, symbol:"m/s"},
     kmh:{factor:0.2777777778, symbol:"km/h"},
     mph:{factor:0.44704, symbol:"mph"},
+    fts:{factor:3.2808399, symbol:"ft/s"},
     knot:{factor:0.514444, symbol:"kn"},
   }
 };
